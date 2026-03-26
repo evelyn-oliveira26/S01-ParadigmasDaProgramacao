b67277a760846a624b6ec1dc50beb3632113b1ef
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-03-26 10:29:45 -0300

    Create Relatório 04 - Exercício 4.go

---
+++ Relatório 04 - Exercício 4.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// função verificar login
+func verificarLogin(usuario, senha string) bool {
+	if usuario == "senha" && senha == "admin" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	var usuario, senha string
+
+	for {
+		fmt.Print("Digite o usuário: ")
+		fmt.Scanln(&usuario)
+
+		fmt.Print("Digite a senha: ")
+		fmt.Scanln(&senha)
+
+		status := verificarLogin(usuario, senha)
+
+		if status {
+			fmt.Println("Login bem-sucedido!")
+			break
+		} else {
+			fmt.Println("Usuário ou senha incorretos. Tente novamente.")
+		}
+	}
+}
