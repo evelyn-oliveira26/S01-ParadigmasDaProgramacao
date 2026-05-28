cac61339eaf80b45e0c6c5d97d455dba2b892b85
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-05-28 11:43:58 -0300

    Create Prato.java

---
+++ Relatório 11 - Java/Exercício 4/Prato.java
@@ -0,0 +1,17 @@
+public class Prato {
+    private String nome;
+    private Monstro ingrediente;
+    private Tempero tempero;
+
+    public Prato(String nome, Monstro monstro, String nomeTempero) {
+        this.nome = nome;
+        this.ingrediente = monstro;
+        this.tempero = new Tempero(nomeTempero);
+    }
+
+    public void servir() {
+        System.out.println("\nPrato Servido: " + nome);
+        ingrediente.virarPrato();
+        System.out.println("Tempero usado: " + tempero.getNome());
+    }
+}
