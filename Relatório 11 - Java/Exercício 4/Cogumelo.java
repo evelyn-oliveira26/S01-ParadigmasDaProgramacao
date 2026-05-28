c7e444b2043f69b8897ff85ec67a12884af0c489
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-05-28 11:42:44 -0300

    Create Cogumelo.java

---
+++ Relatório 11 - Java/Exercício 4/Cogumelo.java
@@ -0,0 +1,10 @@
+public class Cogumelo extends Monstro {
+    public Cogumelo(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void virarPrato() {
+        System.out.println("Senshi fatia e refoga na panela de ferro.");
+    }
+}
