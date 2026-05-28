60c72852795c0c5b503de038fc6567ba60cce538
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-05-28 11:43:01 -0300

    Create Temepro.java

---
+++ Relatório 11 - Java/Exercício 4/Temepro.java
@@ -0,0 +1,11 @@
+public class Tempero {
+    private String nome;
+
+    public Tempero(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
