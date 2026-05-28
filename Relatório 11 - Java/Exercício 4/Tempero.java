9660e81c5ec9793ecf1140dcb7fcf3786b9a352c
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-05-28 13:51:00 -0300

    Update Tempero.java

--- Relatório 11 - Java/Exercício 4/Tempero.java
+++ Relatório 11 - Java/Exercício 4/Tempero.java
@@ -1,11 +1,17 @@
 public class Tempero {
     private String nome;
+    private double quantidade;
 
-    public Tempero(String nome) {
+    public Tempero(String nome, double quantidade) {
         this.nome = nome;
+        this.quantidade = quantidade;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
     }
 }
