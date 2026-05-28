451d3cac094b6962af9313b721504c7d82a39bd8
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-05-28 11:42:26 -0300

    Create Lagosta.java

---
+++ Relatório 11 - Java/Exercício 4/Lagosta.java
@@ -0,0 +1,10 @@
+public class Lagosta extends Monstro {
+    public Lagosta(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void virarPrato() {
+        System.out.println("Senshi ferve até ficar vermelha e extrai a carne macia da cauda.");
+    }
+}
