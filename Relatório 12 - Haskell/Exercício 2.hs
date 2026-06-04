1a2056b4e97af8c05de0836c098f9f5a4c0519ea
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-06-04 13:41:18 -0300

    Create Exercício 2.hs

---
+++ Relatório 12 - Haskell/Exercício 2.hs
@@ -0,0 +1,23 @@
+data Item = Item String String Double
+data StatusCompra = Pendente | Concluida | Cancelada deriving (Eq)
+data Compra = Compra [Item] StatusCompra
+
+totalItens :: [Item] -> Double
+totalItens itens = sum (map (\(Item _ _ preco) -> preco) itens)
+
+valorFinal :: Compra -> Double
+valorFinal (Compra itens status)
+    | status == Cancelada = 0.0
+    | total > 200 = total * 0.9
+    | otherwise = total
+    where total = totalItens itens
+
+main :: IO ()
+main = do
+    let espada = Item "Espada de Mármore" "Arma" 120.0
+    let escudo = Item "Escudo Potente" "Equipamento" 90.0
+    let pocao = Item "Pocao Vermelha" "Poção" 30.0
+
+    let compra = Compra [espada, escudo, pocao] Concluida
+
+    putStrLn $ "Valor final: R$ " ++ show (valorFinal compra)
