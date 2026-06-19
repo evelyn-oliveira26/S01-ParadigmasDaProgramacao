65ff57b4b0a9126dff604a972c9b53765d7be1a6
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-06-19 20:22:20 -0300

    Update Exercício 4.pl

--- Relatório 14/Exercício 4/Exercício 4.pl
+++ Relatório 14/Exercício 4/Exercício 4.pl
@@ -6,3 +6,23 @@ fluxo_energia(A, D) :-
 fluxo_energia(A, D) :- 
     alimenta(A, Z), 
     fluxo_energia(Z, D).
+
+%query teste
+% ?- fluxo_energia(sol, Destino).
+
+%retorno
+% Destino = plantas 
+% Destino = algas 
+% Destino = capivara 
+% Destino = veado 
+% Destino = gafanhoto 
+% Destino = anta 
+% Destino = onca 
+% Destino = jacare 
+% Destino = onca 
+% Destino = sapo 
+% Destino = passaro 
+% Destino = cobra 
+% Destino = gavião 
+% Destino = cobra 
+% Destino = gavião 
