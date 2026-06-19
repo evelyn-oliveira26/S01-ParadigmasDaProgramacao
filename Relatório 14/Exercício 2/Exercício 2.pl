b3733b256df450496927b0fd55b5815b5124947a
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-06-19 20:13:55 -0300

    Update Exercício 2.pl

--- Relatório 14/Exercício 2/Exercício 2.pl
+++ Relatório 14/Exercício 2/Exercício 2.pl
@@ -3,3 +3,9 @@ especie_dominante(Animal) :-
     nicho(Animal, Nicho2),
     Nicho1 \= Nicho2,
     local_comum(Animal, margens).
+
+%query teste
+% ?- especie_dominante(Animal).
+
+%retorno
+% Animal = jacare
