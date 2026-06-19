6cc7ba5466cff85d699a5b36dfe2c45fa11ed989
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-06-19 19:55:22 -0300

    Update Exercício 1.pl

--- Relatório 14/Exercício 1/Exercício 1.pl
+++ Relatório 14/Exercício 1/Exercício 1.pl
@@ -1,3 +1,8 @@
+%fatos adicionais
+alimenta(agua, sucuri).
+alimenta(terra, sucuri).
+
+%regra
 especie_chave(Animal) :-
     (alimenta(agua, Animal) ; alimenta(terra, Animal)),
     (nicho(Animal, rios) ; nicho(Animal, pântano) ; nicho(Animal, profundezas)).
