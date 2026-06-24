164da85851f7110ece1afd100f44ba5d69b71852
Evelyn Oliveira <mymariaevelyn@gmail.com>
2026-06-24 20:07:46 -0300

    Update Exercício 1.pl

--- Relatório 15 - Prolog/Exercício 1/Exercício 1.pl
+++ Relatório 15 - Prolog/Exercício 1/Exercício 1.pl
@@ -1,4 +1,4 @@
-%regra 
+%regra (parte 1)
 peça_crítica(P) :-
     componente(P, Energia, _),
     Energia > 5,
@@ -6,9 +6,15 @@ peça_crítica(P) :-
     precisa(P2, P),
     P1 \= P2.
 
-%query teste 
+%query teste 1
 % ?- peça_crítica(P).
 
-%retorno 
+%retorno 1
 % P = propulsor_ion ;
-% P = modulo_suporte_vida
+% P = modulo_suporte_vida.
+
+%query teste 2 (parte 2)
+% ?- componente(P, 6, sistemas), \+ precisa(_, P).
+
+%retorno 2
+% false.
